a47698c9abab7cdb2c28a68a1470b306c42f6ebc
otto <otto.laitinen@fidera.fi>
2023-02-13 14:12:40 +0200

    cleanup

--- controllers/controller.js
+++ controllers/controller.js
@@ -8,25 +8,3 @@ exports.index = (req, res) => {
 exports.about = (req, res) => {
   res.render("about", { title: "About Page" });
 };
-
-// exports.login = (req, res) => {
-//   res.render("login");
-// };
-
-// exports.loginForm = (req, res) => {
-//   res.render("login");
-// };
-
-// //LOGIN PAGE
-// exports.login = (req, res) => {
-//   // Extract the username and password from the form submissions
-//   const username = req.body.username;
-//   const password = req.body.password;
-
-//   // Check if the username and password match the expected values
-//   if (username === "admin" && password === "password") {
-//     res.redirect("/");
-//   } else {
-//     res.render("login", { errorMessage: "Incorrect username or password" });
-//   }
-// };
--- routes/router.js
+++ routes/router.js
@@ -2,12 +2,7 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/controller");
 
-// Getters
 router.get("/", controller.index);
 router.get("/about", controller.about);
-// router.get("/login", controller.loginForm);
-
-// Setters
-// router.post("/login", controller.login);
 
 module.exports = router;
